57c701e68ce61e6af0013e2493bc9cd65b310ce8
Yuxin Ye <yuxin_ye@protonmail.com>
2022-12-02 10:46:30 +1100

    add post and posts widgets

---
+++ src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,14 @@
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlines,
+  FavoriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "../../components/FlexBetween";
+
+const PostWidget = () => {
+  return <div>PostWidget</div>;
+};
+
+export default PostWidget;
