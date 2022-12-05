9b3da4d4db4a79b6622207095347291ea9b375f4
Yuxin Ye <yuxin_ye@protonmail.com>
2022-12-05 11:49:48 +1100

    add profile page component

--- src/scenes/profilePage/index.jsx
+++ src/scenes/profilePage/index.jsx
@@ -1,5 +1,38 @@
+import { Box, useMediaQuery } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import NavBar from "../../scenes/navbar";
+import FriendListWidget from "../../scenes/widgets/FriendListWidget";
+import MyPostWidget from "../../scenes/widgets/MyPostWidget";
+import PostWidget from "../../scenes/widgets/PostWidget";
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const [user, setUser] = useState(null);
+  const { userId } = useParams();
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  if (!user) return null;
+
+  return (
+    <Box>
+      <NavBar />
+    </Box>
+  );
 };
 
 export default ProfilePage;
