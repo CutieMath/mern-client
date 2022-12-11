86e7f853baf1e53a5b8e04f52058411d2aeb5170
Yuxin Ye <yuxin_ye@protonmail.com>
2022-12-11 12:44:11 +1100

    user profile bug fix

--- src/scenes/profilePage/index.jsx
+++ src/scenes/profilePage/index.jsx
@@ -11,7 +11,7 @@ const ProfilePage = () => {
   const [user, setUser] = useState(null);
   const { userId } = useParams();
   const token = useSelector((state) => state.token);
-  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
   const getUser = async () => {
     const response = await fetch(`http://localhost:3001/users/${userId}`, {
@@ -26,7 +26,7 @@ const ProfilePage = () => {
     getUser();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  if (!user) return null;
+  if (!user) return <div>User not found</div>;
 
   return (
     <Box>
@@ -39,10 +39,7 @@ const ProfilePage = () => {
         justifyContent="center"
       >
         <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
-          <UserWidget
-            userId={userId}
-            picturePath={user.PostsWidgetpicturePath}
-          />
+          <UserWidget userId={userId} picturePath={user.picturePath} />
           <Box m="2rem 0" />
           <FriendListWidget userId={userId} />
         </Box>
