dcbf1a6a21c4a6d28883549e1a12d24fdb64b228
Yuxin Ye <yuxin_ye@protonmail.com>
2022-12-11 12:30:28 +1100

    posts bug found

--- src/scenes/widgets/PostWidget.jsx
+++ src/scenes/widgets/PostWidget.jsx
@@ -64,7 +64,7 @@ const PostWidget = ({
           height="auto"
           alt="post"
           style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-          src={`http://localhost:3001/${picturePath}`}
+          src={`http://localhost:3001/assets/${picturePath}`}
         />
       )}
       <FlexBetween mt="0.25rem">
@@ -106,6 +106,7 @@ const PostWidget = ({
               </Typography>
             </Box>
           ))}
+          <Divider />
         </Box>
       )}
     </WidgetWrapper>
